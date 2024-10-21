1ff5745af6aa05299dadc7a37284dc52fc0348fc
Yash Gaikwad <106992333+whothefisyash@users.noreply.github.com>
2024-10-21 18:07:59 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,8 @@
-
+function fetchGeminiOutput() {
+    fetch("/gemini")
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById("gemini-output").innerText = data;
+      });
+  }
+  
